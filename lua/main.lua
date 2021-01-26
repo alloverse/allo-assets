6101dc47a335058bd58c93355ce47fedb9d0c0b8
Patrik Sjöberg <voxxar@gmail.com>
2021-01-27 00:00:24 +0100

    Bump allonet and cleanup main

--- lua/main.lua
+++ lua/main.lua
@@ -5,41 +5,34 @@ local tablex = require('pl.tablex')
 local pretty = require('pl.pretty')
 
 local client = Client(
-    arg[2], 
+    arg[2],
     "asset_sample"
 )
 
+-- The AssetManager takes care of sending and receiving assets
+local assets = Asset.Manager(client.client)
 
-
+-- Parent of all asset views
 local mainView = ui.View(ui.Bounds(0, 1.5, 0,   1, 1, 1))
 local pos = 0
 
-local assets = Asset.Manager(client.client)
-
+-- For each entry in files.txt
 for line in io.lines("files.txt") do
     print("Adding ".. line)
+    -- We add a FileAsset
     local asset = Asset.File(line)
+    assets:add(asset)
+
+    -- And set up an entity to display it
     local view = Asset.View(asset, ui.Bounds(pos, 0, 0,   1, 0.5, 0.1))
     mainView:addSubview(view)
-    assets:add(asset)
+    
+    -- Move over the next one a bit
     pos = pos + 1.5
 end
 
 
-assets:add(head)
-
 local app = App(client)
-
-app.client.delegates.onConnected = function ()
-    print("Connected!")
-    
-    assets:load("hello", function (name, asset)
-        print("Finished ".. name .. ":", asset.data, asset:id())
-    end)
-end
-
-
-
 app.mainView = mainView
 app:connect()
 app:run()
