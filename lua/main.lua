219e94fa2a49550a98fd48275ca72d60db05bdfb
Patrik Sjöberg <voxxar@gmail.com>
2021-01-27 00:57:07 +0100

    Now with buttons to switch between assets instead of displaying all at once

--- lua/main.lua
+++ lua/main.lua
@@ -4,34 +4,67 @@ local class = require('pl.class')
 local tablex = require('pl.tablex')
 local pretty = require('pl.pretty')
 
-local client = Client(
-    arg[2],
-    "asset_sample"
-)
+local host = arg[2]
+local client = Client(host, "asset_sample" )
 
--- The AssetManager takes care of sending and receiving assets
+-- The AssetManager takes care of sending and receiving assets.
 local assets = Asset.Manager(client.client)
-
--- Parent of all asset views
-local mainView = ui.View(ui.Bounds(0, 1.5, 0,   1, 1, 1))
-local pos = 0
 
 -- For each entry in files.txt
 for line in io.lines("files.txt") do
-    print("Adding ".. line)
-    -- We add a FileAsset
+    -- we add a FileAsset to the manager
     local asset = Asset.File(line)
+    asset.name = line:match(".+/(.+)%..+")
     assets:add(asset)
-
-    -- And set up an entity to display it
-    local view = Asset.View(asset, ui.Bounds(pos, 0, 0,   1, 0.5, 0.1))
-    mainView:addSubview(view)
-    
-    -- Move over the next one a bit
-    pos = pos + 1.5
+    print("Added " .. line .. " as " .. asset.name)
 end
 
+-- Setup a view to display the asset
+local mainView = Asset.View(assets:get(1), ui.Bounds(0, 1.5, 0,   1, 1, 1))
+-- a label to display the name
+local label = ui.Label({
+    bounds = ui.Bounds(0, 0.5, 0,   1, 1, 0.1),
+    text = "Asset Preview",
+    lineheight = 0.1,
+})
 
+-- and navigation buttons to go back and forth between assets
+local prev = ui.Button(ui.Bounds(-1, 0, 0,   0.4, 0.4, 0.1))
+prev.label:setText("<-")
+local next = ui.Button(ui.Bounds( 1, 0, 0,   0.4, 0.4, 0.1))
+next.label:setText("->")
+
+-- Add all the views together
+mainView:addSubview(prev)
+mainView:addSubview(next)
+mainView:addSubview(label)
+
+
+-- Track the current selection
+local current = 1
+
+-- asset switching logic
+function switch(dir)
+    current = current + dir
+    if current < 1 then current = assets:count() end
+    if current > assets:count() then current = 1 end
+    local asset = assets:get(current)
+    mainView:asset(asset)
+    label:setText(asset.name)
+end
+
+prev.onActivated = function ()
+    switch(-1)
+end
+
+next.onActivated = function ()
+    switch(1)
+end
+
+-- Initiate state
+switch(0)
+
+-- Run the app
 local app = App(client)
 app.mainView = mainView
 app:connect()
