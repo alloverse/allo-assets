0801e3e8dcba6deb87cf60b9641d57106f4a1375
Patrik Sjöberg <voxxar@gmail.com>
2021-01-28 03:26:48 +0100

    Nicer structure. Smaller, but a bit wonky, demo models

--- lua/main.lua
+++ lua/main.lua
@@ -6,22 +6,39 @@ local pretty = require('pl.pretty')
 
 local host = arg[2]
 local client = Client(host, "asset_sample" )
+local app = App(client)
 
--- The AssetManager takes care of sending and receiving assets.
-local assets = Asset.Manager(client.client)
+
+-- State for the current selection
+local current = 1
+
+-- List of our assets
+local assets = {}
 
 -- For each entry in files.txt
 for line in io.lines("files.txt") do
-    -- we add a FileAsset to the manager
+    -- we create FileAssets
     local asset = Asset.File(line)
     asset.name = line:match(".+/(.+)%..+")
-    assets:add(asset)
+    table.insert(assets, asset)
     print("Added " .. line .. " as " .. asset.name)
 end
 
--- Setup a view to display the asset
-local mainView = Asset.View(assets:get(1), ui.Bounds(0, 1.5, 0,   1, 1, 1))
--- a label to display the name
+-- The AssetManager takes care of serving assets added to it.
+app.assetManager:add(assets)
+
+-- Setup a view to display the seleted asset
+local assetView = ui.View(ui.Bounds(0, 0, 0,   1, 1, 1))
+assetView.specification = function (self)
+    local spec = View.specification(self)
+    spec.geometry = {
+        type = "asset",
+        name = assets[current]:id(),
+    }
+    return spec
+end
+
+-- Add a label to display the name
 local label = ui.Label({
     bounds = ui.Bounds(0, 0.5, 0,   1, 1, 0.1),
     text = "Asset Preview",
@@ -35,37 +52,47 @@ local next = ui.Button(ui.Bounds( 1, 0, 0,   0.4, 0.4, 0.1))
 next.label:setText("->")
 
 -- Add all the views together
-mainView:addSubview(prev)
-mainView:addSubview(next)
-mainView:addSubview(label)
-
-
--- Track the current selection
-local current = 1
+app.mainView.bounds = ui.Bounds(0, 1.5, 0,  1, 1, 0.1)
+app.mainView.grabbable = true
+app.mainView:addSubview(prev)
+app.mainView:addSubview(next)
+app.mainView:addSubview(label)
+app.mainView:addSubview(assetView)
 
 -- asset switching logic
 function switch(dir)
     current = current + dir
-    if current < 1 then current = assets:count() end
-    if current > assets:count() then current = 1 end
-    local asset = assets:get(current)
-    mainView:asset(asset)
-    label:setText(asset.name)
-end
-
-prev.onActivated = function ()
-    switch(-1)
-end
-
-next.onActivated = function ()
-    switch(1)
+    if current < 1 then current = #assets end
+    if current > #assets then current = 1 end
+    -- Set the new asset name
+    label:setText(assets[current].name)
+    -- Ask the asset view to send its new model information to the server
+    assetView:updateComponents(assetView:specification())
 end
 
 -- Initiate state
 switch(0)
 
+-- Connect buttons
+prev.onActivated = function() switch(-1) end
+next.onActivated = function() switch(1) end
+
+-- Add a little bit of animation
+local animate = true
+app:scheduleAction(0.01, true, function()
+    if app.connected and animate then 
+        assetView.bounds:rotate(3.14/180, 0, 1, 0)
+        assetView:updateComponents(assetView:specification())
+    end
+end)
+
+-- Toggle animation when model is touched
+app.mainView.onInteraction = function (self, inter, body, sender)
+    if body[1] == "poke" and body[2] then
+        animate = not animate
+    end
+end
+
 -- Run the app
-local app = App(client)
-app.mainView = mainView
 app:connect()
 app:run()
