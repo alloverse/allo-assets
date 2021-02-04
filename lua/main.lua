88cee84b0f73ea2594315d6de7387b65d6a9cd3e
Patrik Sjöberg <voxxar@gmail.com>
2021-02-04 17:00:24 +0100

    Skip the files and add more models

--- lua/main.lua
+++ lua/main.lua
@@ -5,41 +5,35 @@ local tablex = require('pl.tablex')
 local pretty = require('pl.pretty')
 
 local client = Client(
-    arg[2], 
+    arg[2],
     "asset_sample"
 )
 
+-- The AssetManager takes care of sending and receiving assets
+local assets = Asset.Manager(client.client)
 
-
+-- Parent of all asset views
 local mainView = ui.View(ui.Bounds(0, 1.5, 0,   1, 1, 1))
 local pos = 0
 
-local assets = Asset.Manager(client.client)
+local files = { "sphere", "helmet", "torus", "cylinder", }
 
-for line in io.lines("files.txt") do
-    print("Adding ".. line)
-    local asset = Asset.File(line)
+-- For each entry in files.txt
+for _, name in ipairs(files) do
+    local file = "assets/"..name..".glb"
+    -- We add a FileAsset
+    local asset = Asset.File(file)
+    assets:add(asset)
+
+    -- And set up an entity to display it
     local view = Asset.View(asset, ui.Bounds(pos, 0, 0,   1, 0.5, 0.1))
     mainView:addSubview(view)
-    assets:add(asset)
+    
+    -- Move over the next one a bit
     pos = pos + 1.5
 end
 
 
-assets:add(head)
-
-local app = App(client)
-
-app.client.delegates.onConnected = function ()
-    print("Connected!")
-    
-    assets:load("hello", function (name, asset)
-        print("Finished ".. name .. ":", asset.data, asset:id())
-    end)
-end
-
-
-
+local app = ui.App(client)
 app.mainView = mainView
-app:connect()
-app:run()
+if app:connect() then app:run() end
